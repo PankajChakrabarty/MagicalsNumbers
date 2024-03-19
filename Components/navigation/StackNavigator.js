1d3d7062fb2a49856415494b3f0c1ab49fedb8f7
Pankaj Chakrabarty <t1chpa00@students.oamk.fi>
2024-03-19 23:19:33 +0200

    partially doen for incorrect attempts

--- Components/navigation/StackNavigator.js
+++ Components/navigation/StackNavigator.js
@@ -1,13 +1,16 @@
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import Start from '../Start';
 import Level1 from '../Level1';
 import Level2 from '../Level2';
 import GameScreen from '../GameScreen';
+import NextLevelScreen from '../NextLevelScreen';
+import Level3 from '../Level3'
+import IncorrectAttemptsScreen from '../IncorrectAttemptsScreen';
 
 
 const StackNavigator = () => {
@@ -19,17 +22,20 @@ const StackNavigator = () => {
         <Stack.Screen
           name="Login"
           component={LoginScreen}
-          options={{headerShown: false}}
+          options={{ headerShown: false }}
         />
         <Stack.Screen
           name="Register"
           component={RegisterScreen}
-          options={{headerShown: false}}
+          options={{ headerShown: false }}
         />
         <Stack.Screen name="Home" component={Start} />
         <Stack.Screen name="Level 1" component={Level1} />
         <Stack.Screen name="Level 2" component={Level2} />
         <Stack.Screen name="Game" component={GameScreen} />
+        <Stack.Screen name=" " component={NextLevelScreen} />
+        <Stack.Screen name="Level 3" component={Level3} />
+        <Stack.Screen name="Incorrect Attempts" component={IncorrectAttemptsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
